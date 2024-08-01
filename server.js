f2156b7d7cae6c9e9c066c41dd80debb73f90ab2
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-31 17:55:15 -0700

    Exercise 2

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
+// Exercise 1
+
 app.get('/greetings/:name', function(req, res) {
   res.send(`Well, hello there, ${req.params.name}`)
+})
+
+// Exercise 2
+
+app.get('/roll/:number', function(req, res) {
+  if (isNaN(req.params.number)) {
+    return res.send("You must specify a number.")
+  }
+  res.send(`You rolled a ${Math.floor(Math.random() * req.params.number) + 1}`)
 })
 
 app.listen(3000, function() {
