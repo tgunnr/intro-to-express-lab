4db0e9a3dcd5bd3b34a616e8c17ea63c38b8eadc
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-31 22:48:42 -0700

    Exercise 4

---
+++ data/shoes.js
@@ -0,0 +1,13 @@
+const shoes = [
+  { name: "Birkenstocks", price: 50, type: "sandal" },
+  { name: "Air Jordans", price: 500, type: "sneaker" },
+  { name: "Air Mahomeses", price: 501, type: "sneaker" },
+  { name: "Utility Boots", price: 20, type: "boot" },
+  { name: "Velcro Sandals", price: 15, type: "sandal" },
+  { name: "Jet Boots", price: 1000, type: "boot" },
+  { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+]
+
+export {
+  shoes
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { collectibles } from './data/collectibles.js'
+import { shoes } from './data/shoes.js'
 
 const app = express()
 
@@ -27,6 +28,22 @@ app.get('/collectibles/:index', function(req, res) {
     res.send(`So, you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`)
   } catch (error) {
     res.send("This item is not yet in stock. Check back soon!")
+  }
+})
+
+// Exercise 4
+
+app.get('/shoes', (req, res) => { 
+  if (req.query['min-price'] && req.query['max-price'] && req.query.type) {
+    let minPrice = req.query['min-price']
+    let maxPrice = req.query['max-price']
+    let shoeType = req.query.type
+    let filteredShoes = shoes.filter((shoe) => {
+      return (shoe.price >= minPrice && shoe.price <= maxPrice && shoe.type === shoeType)
+    })
+    res.send(filteredShoes)
+  } else {
+    res.send(shoes)
   }
 })
 
